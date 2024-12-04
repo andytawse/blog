3a6bc7de946f51ad2b36d98e9be4c7d24127e1fd
Andy Tawse <andy.tawse+work@gmail.com>
2024-12-04 14:44:53 +0000

    Add custom theme
    Completely stripped back with no Drupal CSS or JS.
    
    Added the start of custom styles.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -894,9 +894,9 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
  * Keep this code block at the end of this file to take full effect.
  */
 #
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
 
 // Where to export static files to.
 $settings['tome_static_directory'] = '../static';
